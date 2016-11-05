696fc83babb4854741a29a6ac55b47256db6b919
IgnacioVago <nachos_97@hotmail.com>
2016-11-05 15:40:32 -0300

    Add files via upload

---
+++ RecuperatoriosTP/TP 2/Clase_12_Library/EMarca.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase_12_Library
+{
+    public enum EMarca
+    {
+        BMW,
+        Chevrolet,
+        Ford,
+        Iveco,
+        Scania,
+        Yamaha,
+    }
+}
