2ad5deb3aea6bb261530c4e24119a88891b465fb
IgnacioVago <nachos_97@hotmail.com>
2016-11-29 10:01:21 -0300

    Add files via upload
    Tp numero cuatro finalizado.

---
+++ RecuperatoriosTP/VagoIgnacio.2A.TP4/Archivos/Texto.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Archivos
+{
+    public class Texto : IArchivo<string>
+    {
+        public string direccionDeArchivo;
+        public Texto(string archivo)
+        {
+            this.direccionDeArchivo = archivo;
+        }
+
+        public bool guardar(string datos)
+        {
+            try
+            {
+                StreamWriter escritor = new StreamWriter(this.direccionDeArchivo, true);
+                escritor.WriteLine(datos);
+                escritor.Close();
+
+                return true;
+            }
+            catch (Exception)
+            {
+
+                throw new Exception("NO GUARDA.");
+            }
+ 
+        }
+
+        public bool leer(out List<string> datos)
+        {
+            try
+            {
+                StreamReader lector;
+                lector = new StreamReader(this.direccionDeArchivo);
+
+                datos = new List<string>();
+
+                while (lector.EndOfStream == false)
+                {
+                    datos.Add(lector.ReadLine());
+                }
+
+                lector.Close();
+
+                return true;
+            }
+            catch (Exception)
+            {
+                datos = default(List<string>);
+                throw new Exception("NO LEE.");
+            }
+ 
+        }
+    }
+}
---
+++ RecuperatoriosTP/VagoIgnacio.2A.TP4/Hilo/Descargador.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net; // Avisar del espacio de nombre
+using System.ComponentModel;
+
+namespace Hilo
+{
+    public delegate void EventoDescargaFinalizada(string html);
+    public delegate void EventoPorcentajeDeDescarga(int porcentaje);
+    public class Descargador
+    {
+        private string html;
+        private Uri direccion;
+
+        public Descargador(Uri direccion)
+        {
+            this.html = "";
+            this.direccion = direccion;
+        }
+
+        public void IniciarDescarga()
+        {
+            try
+            {
+                WebClient cliente = new WebClient();
+                cliente.DownloadProgressChanged += this.WebClientDownloadProgressChanged;
+                cliente.DownloadStringCompleted += this.WebClientDownloadCompleted;
+
+                cliente.DownloadStringAsync(this.direccion);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public event EventoPorcentajeDeDescarga PorcentajeDescarga;
+
+        private void WebClientDownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            this.PorcentajeDescarga(e.ProgressPercentage);
+        }
+
+        public event EventoDescargaFinalizada DescargaTerminada;
+        private void WebClientDownloadCompleted(object sender, DownloadStringCompletedEventArgs e)
+        {
+            this.html = e.Result;
+            this.DescargaTerminada(this.html);
+        }
+    }
+}
